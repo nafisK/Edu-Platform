ebb4c154755c62dd9efc2d80762aeafccb4acc63
Nafis Khan <nafisrizwank@gmail.com>
2022-08-18 18:24:38 -0400

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Calender from './Pages/Calender'
+import VideoCall from './Pages/VideoCall'
+import PageNotFound from './Pages/PageNotFound'
+import Login from './Pages/Login'
+
 function App() {
-  return <div className='App'>Hello World</div>
+  return (
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/login' element={<Login />} />
+          <Route path='/profile' element={<Home />} />
+          <Route path='/calender' element={<Calender />} />
+          <Route path='/videocall' element={<VideoCall />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
 export default App
---
+++ client/src/Pages/PageNotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PageNotFound() {
+  return (
+    <div className='text-white bg-gray-800 flex justify-center items-center h-screen text-4xl'>
+      Error 404 - Page not found ¯\_(ツ)_/¯
+    </div>
+  )
+}
