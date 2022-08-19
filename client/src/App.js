23231b0c461c70afbdfc6dee2518ccda159cb603
Edison Hua <iseahound@gmail.com>
2022-08-19 15:13:30 -0400

    Implement Side Navigaton Bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import Cal from './Pages/Cal'
 import VideoCall from './Pages/VideoCall'
 import PageNotFound from './Pages/PageNotFound'
 import Login from './Pages/Login'
+import SideNav from "./Components/SideNav";
 
 function App() {
   return (
     <div className='App'>
+      <SideNav />
       <Router>
         <Routes>
           <Route exact path='/' element={<Home />} />
