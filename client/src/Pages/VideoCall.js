bfde6c8c70f1dc8388e9474864488c6cd84b8a83
RRay6 <rray6@gmu.edu>
2022-08-19 23:48:41 -0400

    VideoCall Feature

--- client/src/Pages/VideoCall.js
+++ client/src/Pages/VideoCall.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import SideNav from '../Components/SideNav'
+import React from "react";
+import SideNav from "../Components/SideNav";
+import Tabs from "../Components/TabComponents/Tabs";
+import Video from "../Components/Video";
 
 export default function VideoCall() {
   return (
-    <div className='ml-[20%]'>
+    <div className="ml-[20%] h-screen">
       <SideNav />
       VideoCall
+      <Tabs />
+      <Video />
     </div>
-  )
+  );
 }
