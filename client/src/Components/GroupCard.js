28b7449edf52d296963d2cc22b0cfb65063046d1
Edison Hua <iseahound@gmail.com>
2022-08-20 14:52:57 -0400

    group add animation

--- client/src/Components/GroupCard.js
+++ client/src/Components/GroupCard.js
@@ -19,9 +19,9 @@ const GroupCard = (props) => {
         <body>{members + " members"}</body>
       </div>
       <span class="groupAdd">
-      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M10.2222 0L10.2222 20M20 10.2222L0 10.2222" stroke="black" stroke-opacity="0.25" stroke-width="2"/>
-      </svg>
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M10.2222 0L10.2222 20M20 10.2222L0 10.2222" stroke="black" stroke-opacity="1" stroke-width="2"/>
+        </svg>
       </span>
     </div>
   );
