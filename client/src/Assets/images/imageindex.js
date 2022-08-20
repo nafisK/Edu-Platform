cbc953a580a781fc13ed7942c134cc5ee5afa187
Edison Hua <iseahound@gmail.com>
2022-08-19 21:50:40 -0400

    Use cropped svgs

--- client/src/Assets/images/imageindex.js
+++ client/src/Assets/images/imageindex.js
@@ -1,6 +1,6 @@
-import ideas1 from './teams-planning-and-discussion-of-new-ideas-by-man-and-woman.svg'
-import ideas2 from './teams-completed-teamwork-of-man-and-women.svg'
-import ideas3 from './teams-women-discussing-graphs-in-teamwork.svg'
+import ideas1 from './teams-planning-and-discussion-of-new-ideas-by-man-and-woman-cropped.svg'
+import ideas2 from './teams-completed-teamwork-of-man-and-women-cropped.svg'
+import ideas3 from './teams-women-discussing-graphs-in-teamwork-cropped.svg'
 
 export {
   ideas1,
